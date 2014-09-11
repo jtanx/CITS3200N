dbf214257de1e8069814f6eff9933dfae2c8cf89
Jeremy Tan <jtanx@users.noreply.github.com>
2014-09-11 15:05:45 +0800

    Switch to using migrations instead of fixtures
    A Django 1.7 ''feature''.

---
+++ api/migrations/0001_initial.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Diary',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', models.DateTimeField()),
+                ('submitted', models.DateTimeField(default=django.utils.timezone.now)),
+                ('entry', models.CharField(max_length=255)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='DiaryType',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=40)),
+                ('description', models.CharField(max_length=255, blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='QuestionResponse',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('entry', models.CharField(max_length=255)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Survey',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=255)),
+                ('description', models.CharField(max_length=255)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='SurveyQuestion',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('number', models.IntegerField()),
+                ('description', models.CharField(max_length=255)),
+                ('qtype', models.CharField(default=b'TXT', max_length=3, choices=[(b'INT', b'Integer'), (b'INS', b'Integer scale'), (b'TXT', b'Text'), (b'CHC', b'Single choice'), (b'MCH', b'Multi choice')])),
+                ('required', models.BooleanField(default=True)),
+                ('choices', models.TextField(null=True, blank=True)),
+                ('parent', models.ForeignKey(to='api.Survey')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='SurveyResponse',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', models.DateTimeField()),
+                ('submitted', models.DateTimeField(default=django.utils.timezone.now)),
+                ('creator', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('survey', models.ForeignKey(to='api.Survey')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AlterUniqueTogether(
+            name='surveyquestion',
+            unique_together=set([('parent', 'number')]),
+        ),
+        migrations.AddField(
+            model_name='questionresponse',
+            name='qid',
+            field=models.ForeignKey(to='api.SurveyQuestion'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='questionresponse',
+            name='rid',
+            field=models.ForeignKey(to='api.SurveyResponse', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='diary',
+            name='dtype',
+            field=models.ForeignKey(to='api.DiaryType'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='diary',
+            name='user',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
+            preserve_default=True,
+        ),
+    ]
---
+++ api/migrations/0002_data.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.contrib.auth.models import User
+from django.core.management import call_command
+
+def add_users(apps, schema_editor):
+    su = User.objects.create_user('admin', 'admin@no.no', 'admin')
+    su.save()
+    
+def add_fixtures(apps, schema_editor):
+    #Well this is... perplexing
+    call_command('loaddata', 'api_data.json')
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_users),
+        migrations.RunPython(add_fixtures)
+    ]
