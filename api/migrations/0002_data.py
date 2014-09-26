adb916044b7e62a47e4e1a686dbfe28255c6cfc0
Jeremy Tan <jtanx@users.noreply.github.com>
2014-09-26 22:45:55 +0800

    Probably fix data import on openshift

--- api/migrations/0002_data.py
+++ api/migrations/0002_data.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 from django.contrib.auth.models import User
 from django.core.management import call_command
+from django.conf import settings
+import os
 
 def add_users(apps, schema_editor):
     su = User.objects.create_superuser('admin', 'admin@no.no', 'admin')
@@ -15,7 +17,10 @@ def add_users(apps, schema_editor):
     
 def add_fixtures(apps, schema_editor):
     #Well this is... perplexing
-    call_command('loaddata', 'api_data.json')
+    if not getattr(settings, 'ON_PAAS', False):
+        call_command('loaddata', 'api_data.json')
+    else:
+        call_command('loaddata', os.environ['OPENSHIFT_REPO_DIR'] + '/wsgi/openshift/api_data.json')
 
 class Migration(migrations.Migration):
 
