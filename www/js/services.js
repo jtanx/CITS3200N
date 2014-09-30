e75b3061d29e77f2caa287975f2094afc78653f5
Hillary Loh <hillaryloh91@gmail.com>
2014-09-30 15:50:23 +0800

    spelling error wooahhh
    fixed it!

--- www/js/services.js
+++ www/js/services.js
@@ -96,7 +96,7 @@ angular.module('starter.services', [])
 .factory('Days', function() {
 
   var days = [
-    { id: 0, name: 'Sunday'}, { id: 1, name: 'Monday'}, { id: 2, name: 'Tuesday'},{ id: 3, name: 'Wednsday'}, 
+    { id: 0, name: 'Sunday'}, { id: 1, name: 'Monday'}, { id: 2, name: 'Tuesday'},{ id: 3, name: 'Wednesday'}, 
 	{ id: 4, name: 'Thursday'}, { id: 5, name: 'Friday'}, { id: 6, name: 'Saturday'},
   ];
   
